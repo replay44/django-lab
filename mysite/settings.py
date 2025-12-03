9f2246d6aea9d4aae718b4681077afa7f7d0d7f4
replay44 <viktorkubas97@gmail.com>
2025-12-03 19:03:16 +0200

    Final Lab 8: Added database config and users

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'mysite_usr',
+        'PASSWORD': 'mysite_pass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
